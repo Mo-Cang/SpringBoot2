07f220794e62ea14b4cf486c9a242a934c74da7c
Mo_Cang <sdd515424@qq.com>
2022-10-17 21:01:56 +0800

    update

--- springboot_redis/src/test/java/com/xthedu/SpringbootRedisApplicationTests.java
+++ springboot_redis/src/test/java/com/xthedu/SpringbootRedisApplicationTests.java
@@ -1,14 +1,42 @@
 package com.xthedu;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
 class SpringbootRedisApplicationTests {
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Test
-    void contextLoads() {
-        System.out.println();
+    void set() {
+        ValueOperations ops = redisTemplate.opsForValue();
+        ops.set("age",16);
+    }
+
+    @Test
+    void get() {
+        ValueOperations ops = redisTemplate.opsForValue();
+        Object age = ops.get("age");
+        System.out.println(age);
+    }
+
+    @Test
+    void hset() {
+        HashOperations ops = redisTemplate.opsForHash();
+        ops.put("keya","a","123");
+    }
+
+    @Test
+    void hget() {
+        HashOperations ops = redisTemplate.opsForHash();
+        Object val = ops.get("keya", "a");
+        System.out.println(val);
     }
 
 }
